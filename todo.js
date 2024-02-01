6d16790ead3b4ec2a036a99ca8373407df21a73f
Fredrik Berglund <fredrik@diam.se>
2024-02-01 11:47:12 +0100

    Update with localstorage

--- todo.js
+++ todo.js
@@ -1,36 +1,61 @@
 const todolist = document.querySelector("#todo-container");
 const list = document.querySelector("#todolist");
 const input = document.querySelector("#newtodo");
-function createTodo() {
-	let newInput = input.value;
+let todos;
+const rawTodos = localStorage.getItem("TODOS");
 
-	if (newInput === "") {
-		return;
-	}
+if (!rawTodos) {
+	todos = [];
+} else {
+	todos = JSON.parse(rawTodos);
+}
+console.log(todos);
+const reversedTodos = todos.slice().reverse();
+reversedTodos.forEach((todo) => {
+	createTodo(todo);
+});
+
+function createTodo(newInput) {
 	const todo = `<div class="todo" onclick="todoclick(event);">\
                     <div class="check"><input type="checkbox" /></div>\
                     <div class="name">${newInput}</div>\
-                    <span class="arrow" onclick="moveTodoUp(this)">↑</span>\
-                    <span class="arrow" onclick="moveTodoDown(this)">↓</span>\
-                    <span class="arrow delete" onclick="deleteTodo(this)">x</span>\
+                    <span class="arrow" onclick="moveTodoUp(this)"><span class="material-symbols-outlined">
+keyboard_arrow_up
+</span></span>\
+                    <span class="arrow" onclick="moveTodoDown(this)"><span class="material-symbols-outlined">
+keyboard_arrow_down
+</span></span>\
+                    <span class="arrow delete" onclick="deleteTodo(this)"><span class="material-symbols-outlined">\
+delete
+</span></span>\
   </div>
                 </div>`;
 
-	list.insertAdjacentHTML("beforeend", todo);
-	input.value = "";
+	list.insertAdjacentHTML("afterend", todo);
 }
 
 input.addEventListener("keydown", function (e) {
 	if (e.keyCode === 13) {
 		e.preventDefault();
-		createTodo();
+		let newInput = input.value;
+
+		if (newInput === "") {
+			return;
+		}
+		createTodo(newInput);
+		input.value = "";
+		todos.unshift(newInput);
+		localStorage.setItem("TODOS", JSON.stringify(todos));
 	}
 });
 
 function todoclick(event) {
 	var clickedElement = event.target.closest(".todo");
 
-	if (event.target.classList.contains("arrow")) {
+	if (
+		event.target.classList.contains("arrow") ||
+		event.target.classList.contains("material-symbols-outlined")
+	) {
 		return;
 	}
 
@@ -50,6 +75,12 @@ function moveTodoUp(arrow) {
 	const previousTodo = todo.previousElementSibling;
 
 	if (previousTodo) {
+		const todoText = todo.querySelector(".name").textContent.trim();
+		const currentIndex = todos.indexOf(todoText);
+		const prevText = previousTodo.querySelector(".name").textContent.trim();
+		const prevIndex = todos.indexOf(prevText);
+		[todos[currentIndex], todos[prevIndex]] = [todos[prevIndex], todos[currentIndex]];
+		localStorage.setItem("TODOS", JSON.stringify(todos));
 		todo.parentNode.insertBefore(todo, previousTodo);
 	}
 }
@@ -59,11 +90,27 @@ function moveTodoDown(arrow) {
 	const nextTodo = todo.nextElementSibling;
 
 	if (nextTodo) {
+		const todoText = todo.querySelector(".name").textContent.trim();
+		const currentIndex = todos.indexOf(todoText);
+		const nextText = nextTodo.querySelector(".name").textContent.trim();
+		const nextIndex = todos.indexOf(nextText);
+		[todos[currentIndex], todos[nextIndex]] = [todos[nextIndex], todos[currentIndex]];
+
+		localStorage.setItem("TODOS", JSON.stringify(todos));
 		todo.parentNode.insertBefore(nextTodo, todo);
 	}
 }
 
 function deleteTodo(arrow) {
 	const todo = arrow.closest(".todo");
-	todo.remove();
+	const todoText = todo.querySelector(".name").textContent.trim();
+	const indexToDelete = todos.indexOf(todoText);
+
+	if (indexToDelete !== -1) {
+		todos.splice(indexToDelete, 1);
+		localStorage.setItem("TODOS", JSON.stringify(todos));
+		todo.remove();
+	} else {
+		console.error("Todo not found in the array");
+	}
 }
